6d6238ad35489dcee55b66654504f5a77c84b028
liangjun.zhong <zhongliangjun1@gmail.com>
2014-11-13 13:20:44 +0800

    add basic config

---
+++ src/main/java/com/dianping/struts/StrutsDemo.java
@@ -0,0 +1,20 @@
+package com.dianping.struts;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Author: liangjun.zhong
+ * Date: 14-11-12
+ * Time: PM10:12
+ * To change this template use File | Settings | File Templates.
+ */
+public class StrutsDemo extends ActionSupport {
+    private static final long serialVersionUID = 1150116003710481553L;
+
+    @Override
+    public String execute() throws Exception {
+
+        return null;
+    }
+}
